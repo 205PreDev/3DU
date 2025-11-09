4d1712779a2c24ffc1d78783ac2c14951a2555ed
unknown <lyh2050609@gmail.com>
2025-11-09 18:43:34 +0900

    index 스피너 및 react 로더 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Hide loader when React is ready
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Mark app as ready after first render
+setTimeout(() => {
+  document.body.classList.add('app-ready')
+}, 100)
